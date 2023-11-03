1cf00dbe9e16914acc4132646b75f07b44769579
joeyy-c <chengxinye@gmail.com>
2023-11-03 18:46:04 +0800

    administrator navbar updated

--- resources/views/layouts/partials/administrator/side-navbar.blade.php
+++ resources/views/layouts/partials/administrator/side-navbar.blade.php
@@ -1,6 +1,5 @@
 @php
-    $user = Auth::user();
-    $isAdmin = optional($user->role)->role_name === 'Admin';
+    $isAdmin = auth()->user()->role->role_name == 'Admin';
 @endphp
 
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
@@ -29,19 +28,21 @@
             <span class="menu-title">Service Availability</span>
         </a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#users" aria-expanded="false" aria-controls="users">
-            <i class="icon-layout menu-icon"></i>
-            <span class="menu-title">Users</span>
-            <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="users">
-            <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="{{ route('vendors.index') }}">Vendors</a></li>
-                <li class="nav-item"> <a class="nav-link" href="{{ route('users.index') }}">Normal Users</a></li>
-                <!-- <li class="nav-item"> <a class="nav-link" href="#">Admin</a></li> -->
-            </ul>
-        </div>
-    </li>
+    @if ($isAdmin)
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#users" aria-expanded="false" aria-controls="users">
+                <i class="icon-layout menu-icon"></i>
+                <span class="menu-title">Users</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="users">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="{{ route('vendors.index') }}">Vendors</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ route('users.index') }}">Normal Users</a></li>
+                    <!-- <li class="nav-item"> <a class="nav-link" href="#">Admin</a></li> -->
+                </ul>
+            </div>
+        </li>
+    @endif
     </ul>
 </nav>
